f0af6266290cfae8c2bcaeb383740572b228c6d2
Distortium <clemergamer@bk.ru>
2024-07-07 04:52:19 +0500

    Base

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void ClickButton()
+    {
+        Debug.Log("Мы нажали на кнопку");
+        //print("Hello World");
+    }
+}
